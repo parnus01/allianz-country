0206aae6028f2d606a0297f9422fb68229976204
Danusorn Taboonyong <danusorn.t@ku.th>
2022-07-28 14:40:51 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+    <div className="pt-20">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+    </Router>
+  )
+}
+
+export default App
